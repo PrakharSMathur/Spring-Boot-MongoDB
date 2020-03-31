f9966619b7946956083f067ab1085c4ced8b2a02
PrakharSMathur <32239055+PrakharSMathur@users.noreply.github.com>
2020-03-31 19:38:32 +0530

    Added validation for existing entries

--- spring-mongodb/src/main/java/com/mongodb/springmongodb/Model/Student.java
+++ spring-mongodb/src/main/java/com/mongodb/springmongodb/Model/Student.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "Student")
 public class Student {
-    @Id //unique value
+    @Id // @GeneratedValue(strategy=GenerationType.IDENTITY) //unique value
     private int id;
     private String stName;
     private String stStream;
--- spring-mongodb/src/main/java/com/mongodb/springmongodb/Resource/StudentController.java
+++ spring-mongodb/src/main/java/com/mongodb/springmongodb/Resource/StudentController.java
@@ -12,31 +12,48 @@ import java.util.Optional;
 public class StudentController {
     @Autowired
     private StudentRepository repository;
-
+/*
+    static String sequence() {
+        int init = 1;
+        String s_init = String.valueOf(init);
+        String val = "A" + s_init;
+        init += 1;
+        return val;
+    }
+*/
     //POST method to add student
     @PostMapping("/addStudent")
-    public String saveStudent(@RequestBody Student student){
-        repository.save(student);
-        return "Added Student : "+ student.getId();
+    public String saveStudent(@RequestBody Student student) {
+        int j = student.getId();
+        if (repository.existsById(j) == false) {
+            //student.setId(sequence());
+            repository.save(student);
+            return "Added Student : " + student.getId();
+        } else {
+            return "ID already exists";
+        }
     }
 
     //GET method to fetch and display all students
     @GetMapping("/findAllStudents")
-    public List<Student> getBooks(){
+    public List<Student> getBooks() {
         return repository.findAll();
     }
 
     //GET method to fetch and display student by ID
     @GetMapping("/findAllStudents/{id}")
-    public Optional<Student> getStudent(@PathVariable int id){
+    public Optional<Student> getStudent(@PathVariable int id) {
         return repository.findById(id);
     }
 
     //DELETE method for deleting student entry by ID
     @DeleteMapping("/delete/{id}")
-    public String deleteStudent(@PathVariable int id){
-        repository.deleteById(id);
-        return "Student deleted : "+id;
+    public String deleteStudent(@PathVariable int id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return "Student deleted : " + id;
+        } else {
+            return "Entry does not exist";
+        }
     }
-
 }
